76fd3a0a3181d7fbc64dfcd4e2b43b6d005dc5d2
AbdulAzizElhag <87284243+AbdulAzizElhag@users.noreply.github.com>
2022-01-22 06:36:48 +0200

    Create Stream_Update.py

---
+++ utils/Stream_Update.py
@@ -0,0 +1,18 @@
+import requests
+from dotenv import load_dotenv
+import os
+import time
+
+#globle variables
+load_dotenv()
+CHAT_ID = os.getenv('CHAT_ID')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+time_interval = 15*60
+
+def send_update(chat_id,msg):
+        url= f"https://api.telegram.org/bot{BOT_TOKEN=}/sendMessage?chat_id={CHAT_ID}&text={msg}"
+        requests.get(url)
+
+while True:
+        send_update(CHAT_ID, f"/stream http://n05.radiojar.com/8s5u5tpdtwzuv?rj-ttl=5&rj-tok=AAABeray1BUAaNFktO4VGQX0CQ")
+        time.sleep(time_interval)
